db5dbdc0511b230f37257448025ddfca4b807a4c
Shane Celis <shane.celis@gmail.com>
2024-12-01 07:47:26 -0500

    excise: Remove unused code.

--- src/bin/d1-1.rs
+++ src/bin/d1-1.rs
@@ -14,36 +14,7 @@ use nom::{
     sequence::{tuple, delimited, separated_pair},
     character::{is_digit, complete::{digit1, multispace0, multispace1, i32}},
 };
-
 use std::collections::BinaryHeap;
-
-// A combinator that takes a parser `inner` and produces a parser that also consumes both leading and
-// trailing whitespace, returning the output of `inner`.
-// pub fn ws<'a, O, E: ParseError<&'a str>, F>(
-//     inner: F,
-// ) -> impl Parser<&'a str, Output = O, Error = E>
-// where
-//     F: Parser<&'a str, Output = O, Error = E>,
-// {
-//     delimited(multispace0, inner, multispace0)
-// }
-
-fn from_int(input: &str) -> Result<i32, ParseIntError> {
-    i32::from_str_radix(input, 10)
-}
-
-// fn is_digit(c: char) -> bool {
-//     c.is_digit(10)
-// }
-
-fn int_parser(input: &str) -> IResult<&str, i32> {
-    i32(input)
-    // map_res(take_while_m_n(2, 2, is_digit),
-    //         from_int)(input)
-    // map_res(digit1, |s: &str| s.parse::<i32>())
-    // map_res(
-    // i32::from_str_radix(input, 10)
-}
 
 fn row_parser(input: &str) -> IResult<&str, (i32, i32)> {
     delimited(multispace0, separated_pair(i32, multispace1, i32), multispace0)(input)
@@ -83,14 +54,6 @@ fn main() -> io::Result<()> {
 #[cfg(test)]
 mod test {
     use super::*;
-    #[test]
-    fn parse_number() {
-        assert_eq!(from_int("10"), Ok(10));
-        assert!(from_int(" 10").is_err());
-        assert!(from_int("10 ").is_err());
-        // assert!(ws(int_parser)("10 ").is_ok());
-    }
-
     #[test]
     fn parse_row() {
         assert_eq!(row_parser("   10   30   "), Ok(("", (10, 30))));
